e6a0f4b4af1a5ce6b2d79c3efa441dd8b5ad71f7
Ruslan <ruslan.naeltok@interstruct.com>
2019-11-20 16:20:42 +0200

    changes

--- dist/api/src/container/identifiers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LOGGER = Symbol.for("Logger");
-exports.CONFIG = Symbol.for("Config");
-exports.AUTH_ADAPTER = Symbol.for("AuthAdapter");
-exports.SESSION_STORAGE = Symbol.for("SessionStorage");
-//# sourceMappingURL=identifiers.js.map
